53072b95acdc47fec7dbce475ebfbe43a5cdacd9
hezhaowu <hezhaowu@kingsoft.com>
2020-06-03 22:37:34 +0800

    save

--- redis-cluster.go
+++ redis-cluster.go
@@ -2,6 +2,10 @@ package redis
 
 import (
 	"errors"
+	"fmt"
+	redigo "github.com/gomodule/redigo/redis"
+	"net"
+	"strings"
 	"sync"
 )
 
@@ -12,12 +16,6 @@ var (
 	ErrUnsupportKeyCount   = errors.New("unsupport script key count, which must be 1 on cluster mode")
 )
 
-type SlotInfo struct {
-	Start   int
-	End     int
-	Address string
-}
-
 type redisCluster struct {
 	*Config
 	*sync.RWMutex
@@ -26,8 +24,265 @@ type redisCluster struct {
 }
 
 func newRedisCluster(c *Config) (ret *redisCluster, err error) {
-
+	ret = &redisCluster{
+		Config:  c,
+		RWMutex: new(sync.RWMutex),
+	}
+	err = ret.UpdateClusterIndexes()
+	if err != nil {
+		return nil, err
+	}
+	return ret, nil
 	return
+}
+
+// 指定写锁. 中间可能会更新cluster indexes
+func (rc *redisCluster) UpdateClusterIndexes() (err error) {
+
+	// 获取最新的slots并比较是否发生变化
+	slots, err := QueryClusterSlots(rc.Config)
+	if err != nil || !IsSlotsChanged(rc.Slots, slots) {
+		return
+	}
+
+	rc.RWMutex.Lock()
+	// 清除旧的连接
+	rc.Close()
+	// 重用旧的内存
+	slen := len(slots)
+	if len(rc.Pools) != slen {
+		rc.Pools = make([]*pool, slen)
+	}
+	if len(rc.Index) != CLUSTER_SLOTS_NUMBER {
+		rc.Index = make([]*redigoPool, CLUSTER_SLOTS_NUMBER)
+	}
+	for i, s := range slots {
+		rc.Pools[i], err = newRedisPool(rc.Config, s.Address)
+		if err != nil {
+			// 如果发生错误,需要级联清除已经创建的其他连接池
+			for j := 0; j < i; j++ {
+				rc.Pools[i].Close()
+			}
+			rc.RWMutex.Unlock()
+			return
+		}
+
+		for j := s.Start; j <= s.End; j++ {
+			rc.Index[j] = rc.Pools[i]
+		}
+	}
+
+	rc.RWMutex.Unlock()
+	return
+}
+
+// 指定读锁. 中间可能会更新cluster indexes
+func (rc *redisCluster) indexRedis(key string) *pool {
+	sl := Slot(key)
+	rc.RWMutex.RLock()
+	ret := rc.Index[sl]
+	rc.RWMutex.RUnlock()
+	return ret
+}
+
+/*--------------------------接口方法----------------------------------*/
+func (rc *redisCluster) Do(cmd string, keysArgs ...interface{}) (reply interface{}, err error) {
+	reply, err = rc.indexRedis(keysArgs[0].(string)).Do(cmd, keysArgs...)
+	if err != nil && IsSlotsError(err) {
+		rc.UpdateClusterIndexes()
+		reply, err = rc.indexRedis(keysArgs[0].(string)).Do(cmd, keysArgs...)
+	}
+	return
+
+}
+
+// 管道批量, 有可能部分成功.
+func (rc *redisCluster) Pi(bf BulkCall, resps ...BulkResp) (err error) {
+	err = rc.indexRedis("").Pi(bf, resps...)
+	if err != nil && IsSlotsError(err) {
+		rc.UpdateClusterIndexes()
+		err = rc.indexRedis("").Pi(bf, resps...)
+	}
+	return
+}
+
+// 事务批量, 要么全部成功, 要么全部失败.
+func (rc *redisCluster) Tx(bf BulkCall, resps ...BulkResp) (err error) {
+	err = rc.indexRedis("").Tx(bf, resps...)
+	if err != nil && IsSlotsError(err) {
+		rc.UpdateClusterIndexes()
+		err = rc.indexRedis("").Tx(bf, resps...)
+	}
+	return
+}
+
+// Publish
+func (rc *redisCluster) Pub(key string, msg interface{}) (err error) {
+	err = rc.indexRedis(key).Pub(key, msg)
+	if err != nil && IsSlotsError(err) {
+		rc.UpdateClusterIndexes()
+		err = rc.indexRedis(key).Pub(key, msg)
+	}
+	return
+}
+
+// Subscribe, 阻塞执行sf直到返回stop或error才会结束
+func (rc *redisCluster) Sub(key string, data SubDataCall, meta SubStatCall) (err error) {
+	err = rc.indexRedis(key).Sub(key, data, meta)
+	if err != nil && IsSlotsError(err) {
+		rc.UpdateClusterIndexes()
+		err = rc.indexRedis(key).Sub(key, data, meta)
+	}
+	return
+
+}
+
+func (rc *redisCluster) Eval(script string, keyCount int, keysArgs ...interface{}) (reply interface{}, err error) {
+	if len(keysArgs) == 0 {
+		return nil, ErrArgumentException
+	}
+
+	reply, err = rc.indexRedis(keysArgs[0].(string)).Eval(script, keyCount, keysArgs)
+	if err != nil && IsSlotsError(err) {
+		rc.UpdateClusterIndexes()
+		reply, err = rc.indexRedis(keysArgs[0].(string)).Eval(script, keyCount, keysArgs)
+	}
+	return
+}
+
+func (rc *redisCluster) Close() {
+	// 关闭操作不用锁,避免等待影响
+	for _, p := range rc.Pools {
+		if p != nil {
+			p.Close()
+		}
+	}
+}
+
+/*--------------------------------集群辅助结构及方法----------------------------*/
+
+type SlotInfo struct {
+	Start   int
+	End     int
+	Address string
+}
+
+func QueryClusterSlots(opt *Config) ([]*SlotInfo, error) {
+
+	var rc redigo.Conn
+	var err error
+	var tcp net.Conn
+	for _, address := range opt.Address {
+		if tcp, err = net.DialTimeout(opt.Network, address, opt.ConnectTimeout); err == nil {
+			rc = redigo.NewConn(tcp, opt.ReadTimeout, opt.WriteTimeout)
+			break
+		}
+	}
+	if err != nil {
+		return nil, err
+	}
+	defer rc.Close()
+
+	if opt.Password != "" {
+		if _, err = rc.Do("AUTH", opt.Password); err != nil {
+			return nil, err
+		}
+	}
+
+	infos, err := redigo.Values(rc.Do("CLUSTER", "SLOTS"))
+	if err != nil {
+		return nil, err
+	}
+
+	/*-------------------------------------------------------------
+	172.31.0.63:7000> CLUSTER SLOTS
+	1) 1) (integer) 0
+	   2) (integer) 5460
+	   3) 1) "172.31.0.63"
+		  2) (integer) 7000
+		  3) "a585e144d73c9ca1c72e0dc14ba13b18eddddf61"
+	   4) 1) "172.31.0.63"
+		  2) (integer) 7003
+		  3) "c079a3b1385faf1d1447b38d43941f75f2411f2b"
+	2) 1) (integer) 5461
+	   2) (integer) 10922
+	   3) 1) "172.31.0.63"
+		  2) (integer) 7001
+		  3) "f0e5fd569ce7eaa63ab71174b7d4ae3cb34452b9"
+	   4) 1) "172.31.0.63"
+		  2) (integer) 7004
+		  3) "b46b3907b9cbde9060715260684b64fe3fdd7729"
+	3) 1) (integer) 10923
+	   2) (integer) 16383
+	   3) 1) "172.31.0.63"
+		  2) (integer) 7002
+		  3) "abfe332e0468b67cc63e4693d94273c4a135b448"
+	   4) 1) "172.31.0.63"
+		  2) (integer) 7005
+		  3) "afb69dec265247d7d51496a7302922b0823a08fb"
+	 --------------------------------------------------------------*/
+	ret := make([]*SlotInfo, len(infos))
+	plen := len(opt.Proxyips)
+	for i, info := range infos {
+		data := info.([]interface{})
+		start, _, _ := Int(data[0], nil)
+		end, _, _ := Int(data[1], nil)
+		addrs := data[2].([]interface{})
+		host, _, _ := String(addrs[0], nil)
+		port, _, _ := Int(addrs[1], nil)
+
+		// 替换成代理IP
+		if plen > 0 {
+			vhost := opt.Proxyips[host]
+			if vhost != "" {
+				host = vhost
+			}
+		}
+		ret[i] = &SlotInfo{
+			Start:   start,
+			End:     end,
+			Address: fmt.Sprintf("%s:%d", host, port),
+		}
+	}
+
+	return ret, nil
+}
+
+func IsSlotsError(err error) bool {
+	if rerr, ok := err.(redigo.Error); ok {
+		msg := rerr.Error()
+		if strings.HasPrefix(msg, "MOVED") || strings.HasPrefix(msg, "ASK") {
+			return true
+		}
+	} else if _, ok := err.(*net.OpError); ok {
+		return true
+	}
+	return false
+}
+
+// 比较新旧slots是否发生变化,避免全局更新索引影响全面
+func IsSlotsChanged(slot1, slot2 []*SlotInfo) bool {
+	slen := 0
+	if slen = len(slot1); slen != len(slot2) {
+		return true
+	}
+
+	flags := make([]bool, slen)
+	for _, s1 := range slot1 {
+		found := false
+		for j, s2 := range slot2 {
+			if !flags[j] && s1.Start == s2.Start {
+				flags[j], found = true, true
+				if s1.End != s2.End || s1.Address != s2.Address {
+					return true
+				}
+			}
+		}
+		if !found {
+			return true
+		}
+	}
+	return false
 }
 
 /*====================slots约定====================*/
